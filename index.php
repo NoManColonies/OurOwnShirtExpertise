966cb1dd3fb79678c0fbff91af75e0848abec89a
chaincpp31 <61736144+chaincpp31@users.noreply.github.com>
2020-03-30 01:43:40 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -38,15 +38,16 @@
             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
               <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
               </ol>
               <div class="carousel-inner">
                 <div class="carousel-item active">
                   <img class="d-block w-100" src="pic/head.png" alt="devbanban">
                 </div>
-				<div class="carousel-item active">
-                  <img class="d-block w-100" src="pic/head1.png" alt="devbanban">
+				<div class="carousel-item">
+                  <img class="d-block w-100" src="pic/head1.jpg" alt="devbanban">
                 </div>
                 <div class="carousel-item">
                   <img class="d-block w-100" src="pic/head2.png" alt="devbanban">
