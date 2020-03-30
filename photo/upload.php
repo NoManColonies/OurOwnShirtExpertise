ac04d5686b30ca37276152548906c2cd2391db8c
NoManColonies <38200442+NoManColonies@users.noreply.github.com>
2020-03-30 17:36:14 +0700

    Add files via upload

--- photo/upload.php
+++ photo/upload.php
@@ -15,28 +15,36 @@ class UploadException extends Exception
         switch ($code) {
             case UPLOAD_ERR_INI_SIZE:
                 $message = "The uploaded file exceeds the upload_max_filesize directive in php.ini";
+                echo $message;
                 break;
             case UPLOAD_ERR_FORM_SIZE:
                 $message = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
+                echo $message;
                 break;
             case UPLOAD_ERR_PARTIAL:
                 $message = "The uploaded file was only partially uploaded";
+                echo $message;
                 break;
             case UPLOAD_ERR_NO_FILE:
                 $message = "No file was uploaded";
+                echo $message;
                 break;
             case UPLOAD_ERR_NO_TMP_DIR:
                 $message = "Missing a temporary folder";
+                echo $message;
                 break;
             case UPLOAD_ERR_CANT_WRITE:
                 $message = "Failed to write file to disk";
+                echo $message;
                 break;
             case UPLOAD_ERR_EXTENSION:
                 $message = "File upload stopped by extension";
+                echo $message;
                 break;
 
             default:
                 $message = "Unknown upload error";
+                echo $message;
                 break;
         }
         return $message;
