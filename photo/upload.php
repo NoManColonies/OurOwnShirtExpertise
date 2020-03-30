57a9d5a3cd4565eb1d81e7ed5ba608ac6d11a69f
NoManColonies <38200442+NoManColonies@users.noreply.github.com>
2020-03-30 17:32:12 +0700

    Add files via upload

--- photo/upload.php
+++ photo/upload.php
@@ -3,6 +3,47 @@
 require_once('../.confiq/confiq.php');
 $statusMsg = '';
 
+class UploadException extends Exception
+{
+    public function __construct($code) {
+        $message = $this->codeToMessage($code);
+        parent::__construct($message, $code);
+    }
+
+    private function codeToMessage($code)
+    {
+        switch ($code) {
+            case UPLOAD_ERR_INI_SIZE:
+                $message = "The uploaded file exceeds the upload_max_filesize directive in php.ini";
+                break;
+            case UPLOAD_ERR_FORM_SIZE:
+                $message = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
+                break;
+            case UPLOAD_ERR_PARTIAL:
+                $message = "The uploaded file was only partially uploaded";
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                $message = "No file was uploaded";
+                break;
+            case UPLOAD_ERR_NO_TMP_DIR:
+                $message = "Missing a temporary folder";
+                break;
+            case UPLOAD_ERR_CANT_WRITE:
+                $message = "Failed to write file to disk";
+                break;
+            case UPLOAD_ERR_EXTENSION:
+                $message = "File upload stopped by extension";
+                break;
+
+            default:
+                $message = "Unknown upload error";
+                break;
+        }
+        return $message;
+    }
+}
+
+// Use
 // File upload path
 $targetDir = "../images/";
 $fileName = basename($_FILES["file"]["name"]);
@@ -23,7 +64,12 @@ if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
                 $statusMsg = "File upload failed, please try again.".$connect->errno;
             }
         }else{
-            $statusMsg = "Sorry, there was an error uploading your file.".$_FILES['file']['error'];
+          if ($_FILES['file']['error'] === UPLOAD_ERR_OK) {
+            //uploading successfully done
+          } else {
+            throw new UploadException($_FILES['file']['error']);
+          }
+          $statusMsg = "Sorry, there was an error uploading your file.".$_FILES['file']['error'];
         }
     }else{
         $statusMsg = 'Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed to upload.';
--- photo2/ajax_php_file.php
+++ photo2/ajax_php_file.php
@@ -13,13 +13,13 @@ echo "Return Code: " . $_FILES["file"]["error"] . "<br/><br/>";
 }
 else
 {
-if (file_exists("images/" . $_FILES["file"]["name"])) {
+if (file_exists("../images/" . $_FILES["file"]["name"])) {
 echo $_FILES["file"]["name"] . " <span id='invalid'><b>already exists.</b></span> ";
 }
 else
 {
 $sourcePath = $_FILES['file']['tmp_name']; // Storing source path of the file in a variable
-$targetPath = "images/".$_FILES['file']['name']; // Target path where file is to be stored
+$targetPath = "../images/".$_FILES['file']['name']; // Target path where file is to be stored
 $result = move_uploaded_file($sourcePath,$targetPath) ; // Moving Uploaded file
 if (!$result) {
   echo $_FILES['file']['error'];
