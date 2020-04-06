d3ef00165e1f7feb39f0c5e31a7d318aa18d0348
NoManColonies <38200442+NoManColonies@users.noreply.github.com>
2020-04-06 08:52:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const trigger = document.querySelector('header');
+const navBar = document.querySelector('nav');
+const menu = document.querySelector('.menu');
+const login = document.querySelector('.login');
+
+const navBarOptions = {
+  rootMargin: "50px 0px 0px 0px"
+};
+
+const navBarObserver = new IntersectionObserver((entries, navBarObserver) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      navBar.classList.add('floating');
+      navBar.classList.remove('fixed');
+      trigger.classList.remove('fixed');
+    } else {
+      navBar.classList.add('fixed');
+      navBar.classList.remove('floating');
+      trigger.classList.add('fixed');
+    }
+  });
+}, navBarOptions);
+
+navBarObserver.observe(trigger);
+
+const searchPreparePage = () => {
+  productHeader.scrollIntoView(true);
+};
+
+const toggleSideMenu = () => {
+  const target = document.querySelector('.side__menu');
+  const background = document.querySelector('#dark1');
+  target.classList.toggle('activeSideMenu');
+  background.classList.toggle('activeDarkenBackground');
+};
+
+const toggleLoginMenu = () => {
+  const target = document.querySelector('.login__menu');
+  const background = document.querySelector('#dark2');
+  target.classList.toggle('activeLoginMenu');
+  background.classList.toggle('activeDarkenBackground');
+};
+
+const productPage = () => {
+  window.location = "product.php";
+};
